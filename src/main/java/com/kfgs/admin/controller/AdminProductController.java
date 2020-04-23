9a6a074c837f9465ec04a2cdc79a0c6013d13acd
mangoL-KFGS <mango_liang@outlook.com>
2020-04-23 09:55:54 +0800

    修改管理员端产品列表

--- src/main/java/com/kfgs/admin/controller/AdminProductController.java
+++ src/main/java/com/kfgs/admin/controller/AdminProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("admin/productController")
-public class productController {
+public class AdminProductController {
 
 
 }
